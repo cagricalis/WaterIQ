21a8868fcbfd656b95e99f4b45b13c2af333e213
Cagri Mehmet Calis <cagricalis@gmail.com>
2025-10-10 15:59:50 +0300

    Initial Commit

---
+++ WaterQualityApp/WaterQualityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterQualityAppApp.swift
+//  WaterQualityApp
+//
+//  Created by Çağrı Mehmet Çalış on 10.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterQualityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterQualityAppTests/WaterQualityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterQualityAppTests.swift
+//  WaterQualityAppTests
+//
+//  Created by Çağrı Mehmet Çalış on 10.10.2025.
+//
+
+import Testing
+@testable import WaterQualityApp
+
+struct WaterQualityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
